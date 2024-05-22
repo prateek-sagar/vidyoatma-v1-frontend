c4e3cde8ecb684092e232ffaf64230b3ab979dda
Prateek <prateeksagar806@gmail.com>
2024-05-22 18:03:20 +0530

    Adding some links from the home to the login and signup account

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Home of Vidyoatma</h1>;
+      <h1 className="text-3xl font-bold underline">Home of Vidyoatma</h1>
+      <Link to={"/register"}>
+        <button>Try for free</button>
+      </Link>
+      <Link to={"/authenticate"}>
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
