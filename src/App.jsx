6328892d1eca902cd76cfd6dc37fde0048c9f392
Prateek <prateeksagar806@gmail.com>
2024-05-22 22:34:18 +0530

    try to use context for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import "./App.css";
+import { createContext } from "react";
+
+const UserContext = createContext();
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Home of Vidyoatma</h1>
-      <Link to={"/register"}>
-        <button>Try for free</button>
-      </Link>
-      <Link to={"/authenticate"}>
-        <button>Login</button>
-      </Link>
-    </div>
+    <UserContext.Provider value={"RaOne"}>
+      <div>
+        <h1 className="text-3xl font-bold underline">Home of Vidyoatma</h1>
+        <Link to={"/register"}>
+          <button>Try for free</button>
+        </Link>
+        <Link to={"/authenticate"}>
+          <button>Login</button>
+        </Link>
+      </div>
+    </UserContext.Provider>
   );
 }
 
