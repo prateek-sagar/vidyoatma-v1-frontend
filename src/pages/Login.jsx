c19aeaa50a773872401a4c1077972c1d501fbe37
Prateek <prateeksagar806@gmail.com>
2024-05-29 11:25:46 +0530

    Fetching the api to the backend and posting the request

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,24 @@ function Login() {
     setData({ ...data, password: e.target.value });
   }
 
-  function authenticate() {
+  async function authenticate() {
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:8080/api/v1/auth/authenticate",
+        {
+          method: "POST",
+          mode: "cors",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+          credentials: "include",
+        }
+      );
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
+    // fetch API
+
     console.log(data);
   }
 
