361f905c2fa2bdfe7edfb83ed3a02006a963b5b5
Prateek <prateeksagar806@gmail.com>
2024-05-30 12:27:14 +0530

    Make the logo as a link

--- src/components/ShowLogo.jsx
+++ src/components/ShowLogo.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function ShowLogo() {
-  return <div className="font-alata text-[24px] md:text-[48px]">Vidyoatma</div>;
+  return (
+    <Link to={"/"} className="font-alata text-[24px] md:text-[48px]">
+      Vidyoatma
+    </Link>
+  );
 }
