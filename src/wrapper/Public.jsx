2d3b07b540c762d7a4ea6bc81f0dc2a5a7561d69
Prateek <prateeksagar806@gmail.com>
2024-06-01 18:35:55 +0530

    Deleting the unnecessary code

--- src/wrapper/Public.jsx
+++ src/wrapper/Public.jsx
@@ -4,7 +4,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Public() {
-  console.log(window.location.pathname);
   return (
     <div className="public-wrapper">
       <Header />
