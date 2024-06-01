643d59f9a26db15d968f7236f6fcf6e953413eb1
Prateek <prateeksagar806@gmail.com>
2024-06-01 18:39:08 +0530

    redesign the dashboard home

--- src/layouts/DashboardHome.jsx
+++ src/layouts/DashboardHome.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Standards from "../components/Standards";
-import InDashboardWrapper from "../components/InDashboardWrapper";
+import "../css/DashboardHome.css";
+import { useUserContext } from "../components/UserContext";
 
 function DashboardHome() {
+  const { user } = useUserContext();
+
   return (
-    <InDashboardWrapper>
-      <div className="px-8 dashboard-institution-name h-[15%]">
-        <p className="text-[40px] institution-name">GOne Test School</p>
-        <p className="institution-city">Sahibabad</p>
+    <div className="home-container">
+      <div className="institution-details">
+        <p>GOne Test School</p>
+        <p>{user.id}</p>
+        <p>Sahibabad</p>
+        <p></p>
       </div>
-      <div className="px-8 grid grid-cols-5 gap-6 py-2 h-[10%] text-base font-medium">
-        <button className="px-3 py-1 bg-gray-200 rounded-lg">
-          Add Student
-        </button>
-        <button className="px-3 py-1 bg-gray-200 rounded-lg">
-          Add Teacher
-        </button>
+      <div className="btn-section">
+        <button className="">Add Student</button>
+        <button className="">Add Teacher</button>
       </div>
-      <div className="px-8 overflow-y-scroll h-[75%] py-4">
+      <div className="">
         <Standards />
       </div>
-    </InDashboardWrapper>
+    </div>
   );
 }
 
