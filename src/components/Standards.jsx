20a4a366cb45abbfdc1ebd5230af9cb12e5fc060
Prateek <prateeksagar806@gmail.com>
2024-06-01 18:30:56 +0530

    Adding the name spelling instead of just displaying the numbers

--- src/components/Standards.jsx
+++ src/components/Standards.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import "../css/DashboardHome.css";
 
+const standards_name = [
+  "First",
+  "Second",
+  "Third",
+  "Fourth",
+  "Fifth",
+  "Sixth",
+  "Seventh",
+  "Eighth",
+  "Nineth",
+  "Tenth",
+  "Eleventh",
+  "Twelfth",
+];
 function Standards() {
   const lowerStandard = 1;
   const higherStandard = 10;
   const standards = [];
   for (let i = lowerStandard; i <= higherStandard; i++) {
-    standards.push({ id: i, standard: i });
+    standards.push({ id: i, standard: standards_name[i - 1] });
   }
   return (
-    <ul className="max-w-[80%] h-full grid gap-2">
+    <ul className="standard-list">
       {standards.map((standard) => (
-        <li
-          key={standard.id}
-          className="px-4 border border-gray-400 flex justify-center items-center hover:cursor-pointer rounded-sm"
-        >
-          <div className="flex  items-center space-x-4 rtl:space-x-reverse w-full">
-            <div className="flex-1 min-w-0">{standard.standard}</div>
-            <div className="inline-flex items-center text-base font-semibold">
-              <IoIosArrowDown />
-            </div>
-          </div>
+        <li key={standard.id} className="">
+          {standard.standard}
+          <IoIosArrowDown />
         </li>
       ))}
     </ul>
