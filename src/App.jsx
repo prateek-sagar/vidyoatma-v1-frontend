9bb126a0c1a61484d4064f29105a89c9deee6863
Prateek <prateeksagar806@gmail.com>
2024-06-01 18:30:02 +0530

    Adding feature provider to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import "./css/App.css";
 import { UserProvider } from "./components/UserContext";
 import { RouterProvider } from "react-router-dom";
 import { Routes } from "./routes/Routes.jsx";
+import { FeatureProvider } from "./components/FeaturesContext.jsx";
 
 export default function App() {
   return (
     <UserProvider>
-      <RouterProvider router={Routes}></RouterProvider>
+      <FeatureProvider>
+        <RouterProvider router={Routes}></RouterProvider>
+      </FeatureProvider>
     </UserProvider>
   );
 }
