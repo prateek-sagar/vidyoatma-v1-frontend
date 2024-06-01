cbadbae72e0417589683d5327ca908eb31c16541
Prateek <prateeksagar806@gmail.com>
2024-06-01 18:40:40 +0530

    delete unnecessary files

--- src/pages/ProtectiveRoutes.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectiveRoutes() {
-  let auth = { token: true };
-  return auth.token ? <Outlet /> : <Navigate to="/login" />;
-}
