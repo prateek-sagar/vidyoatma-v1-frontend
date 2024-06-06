eb31ecc526672ce80a1e000c85b4684753c79c70
Prateek <prateeksagar806@gmail.com>
2024-06-06 15:33:21 +0530

    Removing of userprovider because it is no more use

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./css/App.css";
 
-import { UserProvider } from "./components/UserContext";
 import { RouterProvider } from "react-router-dom";
 import { Routes } from "./routes/Routes.jsx";
 import { FeatureProvider } from "./components/FeaturesContext.jsx";
-
 export default function App() {
   return (
-    <UserProvider>
-      <FeatureProvider>
-        <RouterProvider router={Routes}></RouterProvider>
-      </FeatureProvider>
-    </UserProvider>
+    <FeatureProvider>
+      <RouterProvider router={Routes}></RouterProvider>
+    </FeatureProvider>
   );
 }
