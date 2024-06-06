13d664473d7c13759efa35edd9505342681cefc8
Prateek <prateeksagar806@gmail.com>
2024-06-06 15:42:16 +0530

    Create home when student login

---
+++ src/studentComponent/StudentsHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function StudentsHome() {
+  return (
+    <section>
+      <div className="">
+        <Standards />
+      </div>
+    </section>
+  );
+}
