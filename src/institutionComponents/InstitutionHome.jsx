b8d291e6cb9ba4edba39fd6d48c7dccaf0052aac
Prateek <prateeksagar806@gmail.com>
2024-06-06 15:39:15 +0530

    Creating institution home separate for the modularity

---
+++ src/institutionComponents/InstitutionHome.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import Standards from "./Standards";
+import store from "../redux/store";
+import institutionStore from "../redux/institutionStore";
+import { setInstitution } from "../redux/actions";
+
+const _fetch = {
+  id: "",
+};
+export default function InstitutionHome() {
+  const stored_id = store.getState().id;
+  console.log(stored_id);
+  const jsonData = {
+    participantsId: stored_id,
+  };
+  const [fetched, setFetched] = useState(false);
+  const getUser = async () => {
+    const response = await fetch(
+      "http://localhost:8080/api/v1/get/institution",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+        credentials: "include",
+      }
+    );
+    console.log(response);
+    if (response.ok) {
+      let result = await response.json();
+      // console.log("====================================");
+      // console.log(result.basicInfo.name);
+      // console.log("====================================");
+      institutionStore.dispatch(
+        setInstitution(
+          result.basicInfo.name,
+          result.basicInfo.establishmentDate,
+          result.abreviate,
+          result.address.building_no,
+          result.address.locality,
+          result.address.city,
+          result.address.district,
+          result.address.state,
+          result.address.country,
+          result.lowerStandard,
+          result.higherStandard
+        )
+      );
+    }
+  };
+  useEffect(() => {
+    if (store.getState().isLogin && !fetched) {
+      console.log("fetching....");
+
+      console.log(store.getState().id);
+      console.log(jsonData);
+      getUser();
+      setFetched(true);
+    }
+    return () => {};
+  }, [store.getState().isLogin]);
+  return (
+    <section>
+      <div className="btn-section">
+        <button className="">Add Student</button>
+        <button className="">Add Teacher</button>
+      </div>
+      <div className="">
+        <Standards />
+      </div>
+    </section>
+  );
+}
