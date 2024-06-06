fafaa70981bc250f3af25da86c2b160ef4e212a7
Prateek <prateeksagar806@gmail.com>
2024-06-06 15:42:38 +0530

    Create home when teacher login

---
+++ src/teacherComponents/TeachersHome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TeachersHome() {
+  return (
+    <section>
+      <div className="btn-section">
+        <button className="">Add Student</button>
+      </div>
+      <div className="">
+        <Standards />
+      </div>
+    </section>
+  );
+}
