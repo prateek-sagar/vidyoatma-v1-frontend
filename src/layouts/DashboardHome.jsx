b7ebda62789be22ded5f9bc16b6b302fbe663b46
Prateek <prateeksagar806@gmail.com>
2024-06-07 18:38:29 +0530

    Simplify the dashbaord home

--- src/layouts/DashboardHome.jsx
+++ src/layouts/DashboardHome.jsx
@@ -1,20 +1,9 @@
 import "../css/DashboardHome.css";
 import InstitutionHome from "../institutionComponents/InstitutionHome";
-import store from "../redux/store";
-import institutionStore from "../redux/institutionStore";
 
 function DashboardHome() {
-  console.log(institutionStore.getState());
   return (
     <div className="home-container">
-      {/* user information and link to change it if needed */}
-      <div className="institution-details">
-        <p>{institutionStore.getState().name}</p>
-        <p>{store.getState().id}</p>
-        <p>{institutionStore.getState().city}</p>
-        <p></p>
-      </div>
-      {/* the below section will be changed */}
       <InstitutionHome />
     </div>
   );
