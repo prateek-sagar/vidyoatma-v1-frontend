2229b72428ac12bfeb54aeba1013449fecfd05a5
Prateek <prateeksagar806@gmail.com>
2024-06-07 18:37:10 +0530

    Change of the navigation li to link and add class name and also change in css

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import "../css/Navigation.css";
 import { useFeatureContext } from "../sharedcomponents/FeaturesContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import store from "../redux/store";
 import { removeUser } from "../redux/actions";
 function Sidebar() {
   const [active, setActive] = useState(1);
   const { feature } = useFeatureContext();
   const navigate = useNavigate();
+
   const handlelogout = () => {
     store.dispatch(removeUser());
     console.log("logging out");
@@ -16,11 +17,20 @@ function Sidebar() {
   const handleActiveId = (id) => {
     setActive(id);
   };
-  const _features = [{ id: 1, name: "Home" }];
+  const _features = [{ id: 1, name: "home" }];
   for (let index = 0; index < feature.length; index++) {
     const element = feature[index];
     _features.push({ id: index + 2, name: element });
   }
+  const handleString = (str) => {
+    const strArray = str.split("_");
+    let newStr = "";
+    strArray.forEach((e) => {
+      e = e.charAt(0).toUpperCase() + e.slice(1);
+      newStr = newStr.concat(e.concat(" "));
+    });
+    return newStr;
+  };
   // it is protected and seen after login
   return (
     <div className="sidebar">
@@ -31,14 +41,15 @@ function Sidebar() {
       <nav>
         <ul className="nav-list">
           {_features.map((feature) => (
-            <li
+            <Link
+              to={`${feature.name === `home` ? `` : feature.name}`}
               key={feature.id}
               id={feature.id}
-              className={active === feature.id ? "active" : ""}
+              className={`links ${active === feature.id ? "active" : ""}`}
               onClick={() => handleActiveId(feature.id)}
             >
-              {feature.name}
-            </li>
+              {handleString(feature.name)}
+            </Link>
           ))}
         </ul>
         <div className="sidebar-footer">
