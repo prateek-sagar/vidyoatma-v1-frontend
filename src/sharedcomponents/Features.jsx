96499431f12721245d2645600ce6ba66de0e37fc
Prateek <prateeksagar806@gmail.com>
2024-06-07 18:43:06 +0530

    Creating the features file that will return the required component

---
+++ src/sharedcomponents/Features.jsx
@@ -0,0 +1,36 @@
+import InsAllocation from "../institutionComponents/InsAllocation";
+import InsAttendance from "../institutionComponents/InsAttendance";
+import Attendance from "../institutionComponents/InsAttendance";
+import InsFinance from "../institutionComponents/InsFinance";
+import InsNotifications from "../institutionComponents/InsNotifications";
+import InsStudentPortfolio from "../institutionComponents/InsStudentPortfolio";
+import InsTrackCourse from "../institutionComponents/InsTrackCourse";
+import InsTsunami from "../institutionComponents/InsTsunami";
+import store from "../redux/store";
+
+export function getFeature(params) {
+  switch (store.getState().role) {
+    case "INSTITUTION":
+      switch (params.feature) {
+        case "student_portfolio":
+          return <InsStudentPortfolio />;
+        case "track_course":
+          return <InsTrackCourse />;
+        case "notifications":
+          return <InsNotifications />;
+        case "attendance":
+          return <InsAttendance />;
+        case "finance":
+          return <InsFinance />;
+        case "allocation":
+          return <InsAllocation />;
+        case "tsunami":
+          return <InsTsunami />;
+        default:
+          break;
+      }
+
+    default:
+      break;
+  }
+}
