33c47f189e6d17a8f473d5ba62c4d5d45cf02908
Prateek <prateeksagar806@gmail.com>
2024-08-02 20:32:55 +0530

    Remove features context and use of feature state

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import "../css/Navigation.css";
-import { useFeatureContext } from "../sharedcomponents/FeaturesContext";
 import { Link, useNavigate } from "react-router-dom";
 import store from "../redux/stores/userStore";
 import { removeAllStandard, removeUser } from "../redux/actions";
 import { standardStore } from "../redux/stores/StandardStore";
 function Sidebar() {
   const [active, setActive] = useState(1);
-  const { feature } = useFeatureContext();
+  const features = store.getState().features;
   const navigate = useNavigate();
-
   const handlelogout = () => {
     store.dispatch(removeUser());
     console.log("logging out");
@@ -19,10 +17,16 @@ function Sidebar() {
   const handleActiveId = (id) => {
     setActive(id);
   };
+
+  const handleSetting = () => {
+    navigate("account");
+  };
   const _features = [{ id: 1, name: "home" }];
-  for (let index = 0; index < feature.length; index++) {
-    const element = feature[index];
-    _features.push({ id: index + 2, name: element });
+  if (features) {
+    for (let index = 0; index < features.length; index++) {
+      const element = features[index];
+      _features.push({ id: index + 2, name: element });
+    }
   }
   const handleString = (str) => {
     const strArray = str.split("_");
@@ -56,12 +60,8 @@ function Sidebar() {
         </ul>
       </nav>
       <div className="sidebar-footer">
-        <button
-          className="flex justify-center items-center px-[0.5rem] py-[0.15rem]"
-          onClick={() => handlelogout()}
-        >
-          Logout
-        </button>
+        <button onClick={() => handleSetting()}>Account Settings</button>
+        <button onClick={() => handlelogout()}>Logout</button>
       </div>
     </div>
   );
