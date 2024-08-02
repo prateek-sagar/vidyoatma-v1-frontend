4689ee1ef77103074642b9056f97f67183cdbad4
Prateek <prateeksagar806@gmail.com>
2024-08-02 20:37:18 +0530

    Change of the parameter of the formcontroller() previously we get the event and get the target value at runtime now we pass value as an argument

--- src/controllers/FormController.jsx
+++ src/controllers/FormController.jsx
@@ -1,6 +1,6 @@
 import { openForm } from "../redux/actions";
 import formStore from "../redux/stores/FormStore";
 
-export function formController(e) {
-  formStore.dispatch(openForm(e.target.value));
+export function formController(value) {
+  formStore.dispatch(openForm(value));
 }
